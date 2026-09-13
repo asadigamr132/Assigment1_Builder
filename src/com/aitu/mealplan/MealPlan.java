6f0daf0aa1b349baa4973e4ccce4cc4964a9b8ee
Altair <asadigamr132@gmail.com>
2026-09-13 13:45:44 +0500

    comit2

--- src/com/aitu/mealplan/MealPlan.java
+++ src/com/aitu/mealplan/MealPlan.java
@@ -1,4 +1,29 @@
 package com.aitu.mealplan;
 
 public class MealPlan {
+    private final String name;
+    private final int dailyCalories;
+    private final int dailyProtein;
+    private final int mealsPerDay;
+
+    public MealPlan(
+            String name,
+            int dailyCalories,
+            int dailyProtein,
+            int mealsPerDay
+    ){
+        this.name = name;
+        this.dailyCalories = dailyCalories;
+        this.dailyProtein = dailyProtein;
+        this.mealsPerDay = mealsPerDay;
+    }
+    @Override
+    public String toString() {
+        return "MealPlan{" +
+                "name='" + name + '\'' +
+                ", dailyCalories=" + dailyCalories +
+                ", dailyProtein=" + dailyProtein +
+                ", mealsPerDay=" + mealsPerDay +
+                '}';
+    }
 }
